3553f04af293ba9992fdcc3b4dc2873aa9eb4069
Margarita Ilchenko <multik55555@gmail.com>
2019-04-22 10:29:10 -0400

    file in progress

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name, devoured) VALUES ('Meeses', true);
+INSERT INTO burgers (name, devoured) VALUES ('Bacon McDouble', true);
+INSERT INTO burgers (name, devoured) VALUES ('Bacon Clubhouse Burger',true);
